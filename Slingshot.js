247f1267227689d4fc060d3f55588e096873626c
Paulomi Paul <71573780+PaulomiPaul19@users.noreply.github.com>
2021-01-17 19:56:45 +0530

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -4,12 +4,17 @@ class SlingShot{
             bodyA: bodyA,
             pointB: pointB,
             stiffness: 0.04,
-            length: 10
+            length: 1
         }
         this.pointB=pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
+    
+     attach(body){
+        this.sling.bodyA = body;
+    }
+    
     fly(){
 
         this.sling.bodyA=null;
